989de88f91e3a60dc7062ca98a53c74bc250b949
golshandvr-del <genspark_dev@genspark.ai>
2026-07-23 18:39:17 +0000

    router: import triple_sma_pullback (S211)

--- web_tool/src/router.ts
+++ web_tool/src/router.ts
@@ -27,6 +27,7 @@ import {
 import { confirmScore } from './confirmation_filter'
 import { computeBrooksHigh2, BROOKS_SL_POINT, BROOKS_TP_POINT, BROOKS_MAX_HOLD } from './brooks_high2'
 import { computeSignsOfStrength, SOS_SL_POINT, SOS_TP_POINT, SOS_MAX_HOLD } from './signs_of_strength'
+import { computeTripleSMA, DEFAULT_TRIPLE_SMA } from './triple_sma_pullback'
 
 // آستانهٔ امتیازِ تأیید (از ۵) برای گِیت‌کردنِ ورودِ لایه‌های زمان-محورِ Monday/Turn-of-Month.
 // نشستِ S163: فیلترِ تأییدِ متعامد WR این لایه‌ها را از زیرِ ۴۰٪ به بالای ۴۰٪ رساند
